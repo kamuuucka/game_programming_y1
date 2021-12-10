a8d849cbfe6ea80068cfbb8df2e27d525b7210a1
Kamila Matuszak <kamila.matuszak@list.pl>
2021-12-10 10:50:00 +0100

    Player movement
    Forg is moving on a grid

--- GXPEngine2022BB/GXPEngine/MyGame.cs
+++ GXPEngine2022BB/GXPEngine/MyGame.cs
@@ -4,84 +4,28 @@ using System.Drawing;							// System.Drawing contains drawing tools such as Col
 
 public class MyGame : Game
 {
-    private EasyDraw background;
-	private EasyDraw floor;
-	private AnimationSprite player;
+    private Sprite background;
+	private Player player;
 
-	private float speed = 5f;
-	public MyGame() : base(800, 600, false)		// Create a window that's 800x600 and NOT fullscreen
+	private float speed = 64f;
+	public MyGame() : base(768, 768, false)		// Create a window that's 800x600 and NOT fullscreen
 	{
-		// Draw some things on a canvas:
-		//EasyDraw canvas = new EasyDraw(800, 600);
-		//canvas.Clear(Color.MediumPurple);
-		//canvas.Fill(Color.Yellow);
-		//canvas.Ellipse(width / 2, height / 2, 200, 200);
-		//canvas.Fill(50);
-		//canvas.TextSize(32);
-		//canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		//canvas.Text("Welcome!", width / 2, height / 2);
 
-		// Add the canvas to the engine to display it:
-		//AddChild(canvas);
-		//Console.WriteLine("MyGame initialized");
+		background = new Sprite("background.png");
+		background.SetXY(0, 0);
 
-		background = new EasyDraw(width, height);
-		background.Clear(Color.DarkCyan);
-		background.Fill(Color.DarkCyan);
-
-		floor = new EasyDraw(width, 80);
-		floor.Clear(Color.DarkGreen);
-		floor.Fill(Color.DarkGreen);
-		floor.SetXY(0, height - 80);
-
-		player = new AnimationSprite("dog_sheet.png", 6,2);
-		player.SetXY(width / 2, height - 150);
-		player.SetScaleXY(1.5f, 1.5f);
+		player = new Player(768,768);
 
 		AddChild(background);
-		AddChild(floor);
 		AddChild(player);
 	}
 
-	private void CharacterMovement()
-    {
-		bool isMoving = false;
-
-		if (Input.GetKey(Key.A))
-		{
-			player.Move(-speed, 0);
-			player.Mirror(true, false);
-			isMoving = true;
-		}
-		else if (Input.GetKey(Key.D))
-        {
-			player.Move(speed, 0);
-			player.Mirror(false, false);
-			isMoving = true;
-        }
-        else
-        {
-			isMoving = false;
-        }
-
-		if (isMoving)
-        {
-			player.SetCycle(0, 5);
-        }
-        else
-        {
-			player.SetCycle(6, 4);
-        }
-
-		player.Animate(0.1f);
-
-
-	}
+	
 
 	// For every game object, Update is called every frame, by the engine:
 	void Update()
 	{
-		CharacterMovement();
+		player.CharacterMovement();
 	}
 
 	static void Main()							// Main() is the first method that's called when the program is run
---
+++ GXPEngine2022BB/GXPEngine/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    internal class Player : Sprite
+    {
+        private int screen_width;
+        private int screen_height; 
+        private float speed = 64f;
+        public Player(int screen_width, int screen_height) : base("forg.png")
+        {
+            this.screen_height = screen_height;
+            this.screen_width = screen_width;
+            SpawnPlayer();
+        }
+
+        public void CharacterMovement()
+        {
+            bool isMoving = false; //Can be used to create idle and moving animation
+
+            if (Input.GetKeyUp(Key.A))
+            {
+                Move(-speed, 0);
+                Mirror(true, false);
+                isMoving = true;
+            }
+            else if (Input.GetKeyUp(Key.D))
+            {
+                Move(speed, 0);
+                Mirror(false, false);
+                isMoving = true;
+            }
+            else if (Input.GetKeyUp(Key.W))
+            {
+                Move(0, -speed);
+            }
+            else if (Input.GetKeyUp(Key.S))
+            {
+                Move(0, speed);
+            }
+            else
+            {
+                isMoving = false;
+            }
+
+        }
+
+        private void SpawnPlayer()
+        {
+            SetXY(screen_width / 2 + 32, screen_height - 26);
+            SetOrigin(width/2, height/2);
+            Console.WriteLine(screen_width + " : " + screen_height);
+        }
+    }
+}
